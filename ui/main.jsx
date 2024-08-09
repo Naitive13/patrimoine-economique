1d3a10c0447f7e2a90299c67279b46ffa3b1d4fb
Naitive13 <hei.titan.2@gmail.com>
2024-08-09 23:26:09 +0300

    fix: undefined bugs

--- ui/main.jsx
+++ ui/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
 import DataTable from "./Tableau";
+import GetValeur from "./GetValeur";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <DataTable />
+    <GetValeur />
   </React.StrictMode>,
 );
