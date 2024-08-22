bba2e1e225746db7323e4272b7f6f841f16c112a
Naitive13 <hei.titan.2@gmail.com>
2024-08-22 17:24:27 +0300

    chore: reset the design

--- ui/main.jsx
+++ ui/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./Header";
-import DataTable from "./Tableau";
-import GetValeur from "./GetValeur";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <DataTable />
-    <GetValeur />
+    <App />
   </React.StrictMode>,
 );
