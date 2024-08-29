87ac27aed95e9debda81a3267d77fd90192ab4ce
Naitive13 <hei.titan.2@gmail.com>
2024-08-29 16:06:11 +0300

    feat: add react-router

--- ui/App.jsx
+++ ui/App.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./layout";
+import Possession from "./pages/Possession";
+import Patrimoine from "./pages/Patrimoine";
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Possession />,
+  },
+  {
+    path: "/patrimoine",
+    element: <Patrimoine />,
+  },
+  {
+    path: "/possession",
+    element: <Possession />,
+  },
+]);
 
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Layout>
+        <RouterProvider router={route} />
+      </Layout>
     </>
   );
 }
