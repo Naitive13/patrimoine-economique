3b8e11f016dbedc84ea0b7e57732adb269339804
Naitive13 <hei.titan.2@gmail.com>
2024-08-30 19:30:37 +0300

    feat: add Possession page to see all possessions

--- ui/pages/Possession/Possession.jsx
+++ ui/pages/Possession/Possession.jsx
@@ -1,11 +1,24 @@
-import { ButtonGroup } from "react-bootstrap";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../layout";
+import List from "../../components/List";
 
 export default function Possession() {
+  const [possessions, setpossessions] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/possession")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const item = data[1].data.possessions;
+        setpossessions(item);
+      });
+  }, []);
+
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <List possessions={possessions} />
+      </Layout>
     </>
   );
 }
