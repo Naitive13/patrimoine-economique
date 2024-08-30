727d39b1f0ab92f0dd0a2682cbd074828f5928de
Naitive13 <hei.titan.2@gmail.com>
2024-08-30 18:33:47 +0300

    chore: move Possession to a new folder

--- ui/pages/Possession/Possession.jsx
+++ ui/pages/Possession/Possession.jsx
@@ -1,13 +1,11 @@
 import { ButtonGroup } from "react-bootstrap";
 import React from "react";
-import Layout from "../layout";
+import Layout from "../../layout";
 
 export default function Possession() {
   return (
     <>
-      <Layout>
-        <ButtonGroup>Hello possession</ButtonGroup>
-      </Layout>
+      <Layout></Layout>
     </>
   );
 }
