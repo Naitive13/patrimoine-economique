f40396147af189b8f4b51d808d0a11a2264fbdd9
Naitive13 <hei.titan.2@gmail.com>
2024-09-05 17:15:10 +0300

    feat: add new data.json

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.get("/possession", (req, res) => {
   res.set({
     "Content-Type": "application/json",
   });
-  res.sendFile(path.join(__dirname, "../data/data.json"));
+  res.sendFile(path.join(__dirname, "./data/data.json"));
 });
 
 app.post("/possession", (req, res) => {
